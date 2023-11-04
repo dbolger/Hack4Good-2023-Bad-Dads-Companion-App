8b6df793ab49cc0107763c6360e59448cea07e55
daniel <dan@mooresolutions.io>
2023-11-03 21:46:34 -0500

    init

---
+++ H4GGD2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4GGD2023
+//
+//  Created by Daniel Moore on 11/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4GGD2023/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4GGD2023App.swift
+//  H4GGD2023
+//
+//  Created by Daniel Moore on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct H4GGD2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
