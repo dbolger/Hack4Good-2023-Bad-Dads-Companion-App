29ac6f6b46409ef8d335421d9f010ff0cbc68bad
daniel <dan@mooresolutions.io>
2023-11-03 22:13:28 -0500

    init

---
+++ H4GGD2023/RequestManager.swift
@@ -0,0 +1,12 @@
+//
+//  RequestManager.swift
+//  H4GGD2023
+//
+//  Created by Daniel Moore on 11/3/23.
+//
+
+import Foundation
+
+class RequestManager {
+    static let shared = RequestManager()
+}
